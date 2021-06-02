bbc76280b58dd460fd90a3dff950a97fc78af113
muhafidhh <83868302+muhafidhh@users.noreply.github.com>
2021-06-02 23:50:11 +0700

    Update MahasiswaController.php
    Belajar Migration, DB Facade, Query Builder dan Eloquent

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -3,67 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MahasiswaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function insert()
     {
-        return array(
-            "pesan" => "List Data Mahasiswa", 
-            "data" => array()
-        );
+        $result = DB::insert('insert into mahasiswas (npm, nama_mahasiswa, tempat_lahir, tanggal_lahir, 
+        alamat, created_at) values (?, ?, ?, ?, ?, ?)', ['1822240097','Hafidh','Palembang',
+        '2000-03-22','Jl Green Palm', now()]);
+        dump($result);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function update()
     {
-        //
+        $result = DB::update('update mahasiswas set nama_mahasiswa = "Zixy",updated_at = now() where npm = ?', ['2024240022']);
+        dump($result);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function delete()
     {
-        return array(
-            "pesan" => "Detail Mahasiswa dengan id : $id", 
-            "data" => array()
-        );
+        $result = DB::delete('delete from mahasiswas where npm = ?', ['2024240022']);
+        dump($result);
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    
+    public function select()
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $result = DB::select("select * from mahasiswas");
+        dump($result);
     }
 }
+
+
