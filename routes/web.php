23de67516769da9ff4ecd0eb018f3a9938b08e1b
muhafidhh <83868302+muhafidhh@users.noreply.github.com>
2021-06-02 23:48:29 +0700

    Update web.php
    Belajar Migration, DB Facade, Query Builder dan Eloquent

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\MahasiswaController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/mahasiswa/insert', [MahasiswaController::class, 'insert']);
+Route::get('/mahasiswa/update', [MahasiswaController::class, 'update']);
+Route::get('/mahasiswa/delete', [MahasiswaController::class, 'delete']);
+Route::get('/mahasiswa/select', [MahasiswaController::class, 'select']);
+
 
 Route::get('/', function () {
     return view('welcome');
